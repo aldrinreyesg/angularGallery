7ba6a56e52cc3436d5699a691db7d2ee8171b0e0
Aldrin Reyes <aldrin.reyes@gmail.com>
2019-01-25 01:23:29 -0300

    api login enabled

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(cookieParser('secret'));
 app.use(flash());
 app.use(session({
