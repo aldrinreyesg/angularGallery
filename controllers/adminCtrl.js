c750a3ed4bc1570612df3daee4d04fd71960785c
Aldrin Reyes <aldrin.reyes@gmail.com>
2019-02-06 01:09:34 -0300

    insert and delete

--- controllers/adminCtrl.js
+++ controllers/adminCtrl.js
@@ -17,7 +17,7 @@ angular
         $scope.btnAdd = function () {
             $scope.modalTitle = "Agregar Usuario";
             // $scope.modalText = "Está seguro de eliminar estos elementos?";
-            $scope.btnAccept = "Guardar";
+            $scope.btnEnviar = "Guardar";
             $scope.userForm.$setPristine();
             $scope.userForm.$setUntouched();
             // $scope.userForm = {
@@ -27,7 +27,7 @@ angular
             //     email : ""
             // };
             angular.element("#modalUpdate").modal('toggle');
-        };
+        }
 
         $scope.btnBorrar = function () {
             $scope.modalTitle = "Borrar elementos";
@@ -35,18 +35,56 @@ angular
             $scope.btnAccept = "Eliminar";
             angular.element("#table-options").addClass("d-none");
             angular.element("#modalMsg").modal('toggle');
-        };
+        }
+
+        $scope.btnSend = function () {
+            // console.log("validando formulario");
+            console.log($scope.userForm.$valid);
+            if($scope.userForm.$valid){
+                var user = {
+                    user : {
+                        username: $scope.username,
+                        password: $scope.pass1,
+                        email: $scope.email,
+                        role: $scope.role,
+                        enabled: $scope.enabled,
+                    }
+                };
+                saveData(user);
+                angular.element("#modalProgessBar").removeClass('fade');
+                $scope.msgText = "Usuario creado.";
+                angular.element("#ajax-msg").addClass("alert-success");
+                angular.element("#ajax-msg").removeClass("d-none");
+                angular.element("#ajax-msg").removeClass("fade");
+                // angular.element("#modalUpdate").modal('toggle');
+            }
+        }
+
 
         angular.element("#btnAccept").on('click', function () {
             console.log("eliminar");
             var selected = angular.element("#table").bootstrapTable('getSelections');
+            // var users = [];
+            var ids = [];
             angular.forEach(selected, function (value, key) {
                 console.log(value._id);
                 angular.element("#table").bootstrapTable('remove', {
                     field: '_id',
                     values: [value._id]
                 });
+                ids.push(value._id);
             });
+            var users = {
+                user: {
+                    ids: ids
+                }
+            };
+            console.log(JSON.stringify(users));
+            deleteData(users);
+            $scope.msgText = "Usuario eliminado.";
+            angular.element("#ajax-msg").addClass("alert-success");
+            angular.element("#ajax-msg").removeClass("d-none");
+            angular.element("#ajax-msg").removeClass("fade");
 
             angular.element("#modalMsg").modal('toggle');
         });
@@ -75,22 +113,51 @@ angular
             });
         }
 
-
-    }])
-    .directive("compareTo", function () {
-        return {
-            require: "ngModel",
-            scope:
-                {
-                    passwordRepeat: "=compareTo"
-                },
-            link: function (scope, element, attributes, paramval) {
-                paramval.$validators.compareTo = function (val) {
-                    return val == scope.passwordRepeat;
-                };
-                scope.$watch("passwordRepeat", function () {
-                    paramval.$validate();
+        function saveData(user){
+            var token = 'Token ' + $cookies.get('token');
+            header = {
+                'Content-Type': 'application/json',
+                'Authorization': token
+            };
+            $http({
+                method: 'POST',
+                url: 'http://localhost:3000/api/security/create',
+                data: JSON.stringify(user),
+                headers: header,
+                timeout: 15000
+            }).then(function mySuccess(response) {
+                var data = response.data.users;
+                angular.element('#table').bootstrapTable('destroy');
+                angular.element('#table').bootstrapTable({
+                    data: data
                 });
-            }
-        };
-    });
+                console.log("datos cargados");
+            }, function myError(response) {
+                console.log(response);
+            });
+        }
+
+        function deleteData(user){
+            var token = 'Token ' + $cookies.get('token');
+            header = {
+                'Content-Type': 'application/json',
+                'Authorization': token
+            };
+            $http({
+                method: 'POST',
+                url: 'http://localhost:3000/api/security/remove',
+                data: JSON.stringify(user),
+                headers: header,
+                timeout: 15000
+            }).then(function mySuccess(response) {
+                var data = response.data.users;
+                angular.element('#table').bootstrapTable('destroy');
+                angular.element('#table').bootstrapTable({
+                    data: data
+                });
+                console.log("datos cargados");
+            }, function myError(response) {
+                console.log(response);
+            });
+        }
+    }]);
